3938831c255f02d7e47e3158f1d3394e2c2e8225
nhub9889 <nhub9889@gmail.com>
2026-01-20 00:44:50 +0700

    remove bug

--- src/data/coco/__init__.py
+++ src/data/coco/__init__.py
@@ -4,6 +4,4 @@ from .coco_dataset import (
     mscoco_label2category,
     mscoco_category2name,
 )
-from .coco_eval import *
-
-from .coco_utils import get_coco_api_from_dataset
+# from .coco_eval import *
